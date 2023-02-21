c03eb654d38fddac3c5b79130dcf93dbda3c25c1
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-02-20 22:19:05 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(array){
+   const x = array.find((y)=>y.result === "W");
+   if( x === undefined){
+   return x;}
+   else{
+   return  x.year;}
+}
